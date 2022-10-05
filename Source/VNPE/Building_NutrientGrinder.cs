50d64f02f74574ec19af73a7bc6e549d53e0d6bc
KylianB <kylian.balde@gmail.com>
2022-10-05 13:02:25 +0200

    grinder/feeder start

---
+++ Source/VNPE/Building_NutrientGrinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using PipeSystem;
+using RimWorld;
+using Verse;
+
+namespace VNPE
+{
+    public class Building_NutrientGrinder : Building
+    {
+        private List<IntVec3> hoppers;
+
+        public CompPowerTrader powerComp;
+        public CompResource resourceComp;
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+            powerComp = GetComp<CompPowerTrader>();
+            resourceComp = GetComp<CompResource>();
+            hoppers = new List<IntVec3>();
+
+            var adjs = GenAdj.CellsAdjacentCardinal(this);
+            foreach (var cell in adjs)
+            {
+                var ed = cell.GetFirstBuilding(map);
+                if (ed != null && ed.def == ThingDefOf.Hopper)
+                {
+                    UpdateHopper(cell);
+                }
+            }
+        }
+
+        public void UpdateHopper(IntVec3 pos)
+        {
+            Log.Message($"Adding hopper {pos}");
+            if (hoppers.Contains(pos))
+                hoppers.Remove(pos);
+
+            hoppers.Add(pos);
+        }
+    }
+}
---
+++ Source/VNPE/CompProperties_RegisterToGrinder.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace VNPE
+{
+    public class CompProperties_RegisterToGrinder : CompProperties
+    {
+        public CompProperties_RegisterToGrinder()
+        {
+            compClass = typeof(CompRegisterToGrinder);
+        }
+    }
+
+    public class CompRegisterToGrinder : ThingComp
+    {
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            TryUpdate(parent.Map);
+        }
+
+        public override void PostDeSpawn(Map map)
+        {
+            base.PostDeSpawn(map);
+            TryUpdate(map);
+        }
+
+        private void TryUpdate(Map map)
+        {
+            var grinders = new List<Building_NutrientGrinder>();
+            var adjs = GenAdj.CellsAdjacentCardinal(parent);
+
+            foreach (var cell in adjs)
+            {
+                var ed = cell.GetFirstBuilding(map);
+                if (ed != null && ed is Building_NutrientGrinder grinder && !grinders.Contains(grinder))
+                {
+                    grinders.Add(grinder);
+                }
+            }
+
+            var pos = parent.Position;
+            for (int i = 0; i < grinders.Count; i++)
+            {
+                Log.Message($"Trying to add hopper {pos}");
+                grinders[i].UpdateHopper(pos);
+            }
+        }
+    }
+}
