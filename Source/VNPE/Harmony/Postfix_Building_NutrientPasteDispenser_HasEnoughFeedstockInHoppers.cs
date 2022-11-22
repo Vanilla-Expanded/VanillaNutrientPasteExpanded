ef44be86ba300f1096d1290a0403338d51092dbd
KylianB <kylian.balde@gmail.com>
2022-11-22 11:03:53 +0100

    paste dispenser can be used with net

---
+++ Source/VNPE/Harmony/Postfix_Building_NutrientPasteDispenser_HasEnoughFeedstockInHoppers.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using PipeSystem;
+using RimWorld;
+
+namespace VNPE
+{
+    [HarmonyPatch(typeof(Building_NutrientPasteDispenser))]
+    [HarmonyPatch("HasEnoughFeedstockInHoppers", MethodType.Normal)]
+    public static class Postfix_Building_NutrientPasteDispenser_HasEnoughFeedstockInHoppers
+    {
+        public static void Postfix(Building_NutrientPasteDispenser __instance, ref bool __result)
+        {
+            if (!__result)
+            {
+                var comp = __instance.GetComp<CompResource>();
+                if (comp != null && comp.PipeNet is PipeNet net && net.Stored >= 1)
+                    __result = true;
+            }
+        }
+    }
+}
---
+++ Source/VNPE/Harmony/Prefix_Building_NutrientPasteDispenser_TryDispenseFood.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using PipeSystem;
+using RimWorld;
+using Verse;
+using Verse.Sound;
+
+namespace VNPE
+{
+    [HarmonyPatch(typeof(Building_NutrientPasteDispenser))]
+    [HarmonyPatch("TryDispenseFood", MethodType.Normal)]
+    public static class Prefix_Building_NutrientPasteDispenser_TryDispenseFood
+    {
+        public static bool Prefix(Building_NutrientPasteDispenser __instance, ref Thing __result)
+        {
+            if (!__instance.CanDispenseNow)
+            {
+                __result = null;
+                return false;
+            }
+
+            if (__instance.FindFeedInAnyHopper() == null)
+            {
+                var comp = __instance.GetComp<CompResource>();
+                if (comp != null && comp.PipeNet is PipeNet net && net.Stored >= 1)
+                {
+                    __instance.def.building.soundDispense.PlayOneShot(new TargetInfo(__instance.Position, __instance.Map));
+                    net.DrawAmongStorage(1, net.storages);
+
+                    __result = ThingMaker.MakeThing(ThingDefOf.MealNutrientPaste);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
