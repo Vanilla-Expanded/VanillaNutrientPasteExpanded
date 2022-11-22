04540baab19add3073054525f226846f64cbd006
KylianB <kylian.balde@gmail.com>
2022-11-22 09:34:40 +0100

    supppress hopper alert when connected to network

---
+++ Source/VNPE/Harmony/Postfix_Alert_PasteDispenserNeedsHopper_BadDispensers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using PipeSystem;
+using RimWorld;
+using Verse;
+
+namespace VNPE
+{
+    [HarmonyPatch(typeof(Alert_PasteDispenserNeedsHopper))]
+    [HarmonyPatch("BadDispensers", MethodType.Getter)]
+    public static class Postfix_Alert_PasteDispenserNeedsHopper_BadDispensers
+    {
+        public static void Postfix(ref List<Thing> ___badDispensersResult)
+        {
+            ___badDispensersResult.RemoveAll(d => d.TryGetComp<CompResource>() is CompResource cr && cr.PipeNet.connectors.Count > 1);
+        }
+    }
+}
