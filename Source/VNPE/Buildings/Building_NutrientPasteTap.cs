3c541f4ef7e7762ac1f9c1723fd0e4d753a0ff02
Taranchuk <nfhfyxer@gmail.com>
2022-12-31 17:23:29 +0200

    transpiler

--- Source/VNPE/Buildings/Building_NutrientPasteTap.cs
+++ Source/VNPE/Buildings/Building_NutrientPasteTap.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using HarmonyLib;
 using PipeSystem;
 using RimWorld;
 using UnityEngine;
@@ -7,14 +8,26 @@ using Verse.Sound;
 
 namespace VNPE
 {
-    public class Building_NutrientPasteTap : Building
+    public class Building_NutrientPasteTap : Building_NutrientPasteDispenser
     {
         public CompResource resourceComp;
-        public CompPowerTrader powerComp;
-
         public override Color DrawColor => !this.IsSociallyProper(null, false) ? Building_Bed.SheetColorForPrisoner : base.DrawColor;
 
-        public bool CanDispenseNow => powerComp.PowerOn && resourceComp.PipeNet is PipeNet net && net.Stored >= 1;
+        [HarmonyPatch(typeof(Building_NutrientPasteDispenser), "CanDispenseNow", MethodType.Getter)]
+        public static class Building_NutrientPasteDispenser_CanDispenseNow_Patch
+        {
+            public static bool Prefix(ref bool __result, Building_NutrientPasteDispenser __instance)
+            {
+                if (__instance is Building_NutrientPasteTap nutrientPasteTap)
+                {
+                    __result = nutrientPasteTap.CanDispenseNowOverride;
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public bool CanDispenseNowOverride => powerComp.PowerOn && resourceComp.PipeNet is PipeNet net && net.Stored >= 1;
 
         public override void SpawnSetup(Map map, bool respawningAfterLoad)
         {
@@ -34,13 +47,25 @@ namespace VNPE
             return builder.ToString().Trim();
         }
 
-        public Thing TryDispenseFood()
+        [HarmonyPatch(typeof(Building_NutrientPasteDispenser), "TryDispenseFood")]
+        public static class Building_NutrientPasteDispenser_TryDispenseFood_Patch
+        {
+            public static bool Prefix(ref Thing __result, Building_NutrientPasteDispenser __instance)
+            {
+                if (__instance is Building_NutrientPasteTap nutrientPasteTap)
+                {
+                    __result = nutrientPasteTap.TryDispenseFoodOverride();
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public Thing TryDispenseFoodOverride()
         {
             var net = resourceComp.PipeNet;
             def.building.soundDispense.PlayOneShot(new TargetInfo(Position, Map));
-
             net.DrawAmongStorage(1, net.storages);
-
             return ThingMaker.MakeThing(ThingDefOf.MealNutrientPaste);
         }
     }
---
+++ Source/VNPE/Harmony/ThingListGroupHelper_Includes_Patch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using Verse;
+
+namespace VNPE
+{
+    [HarmonyPatch(typeof(ThingListGroupHelper), "Includes")]
+    public static class ThingListGroupHelper_Includes_Patch
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> codeInstructions)
+        {
+            var typeFromHandle = AccessTools.Method(typeof(Type), "GetTypeFromHandle");
+            var isDerivedFrom = AccessTools.Method(typeof(ThingListGroupHelper_Includes_Patch), "IsDerivedFrom");
+            var codes = codeInstructions.ToList();
+            for (var i = 0; i < codes.Count; i++)
+            {
+                var code = codes[i];
+                yield return code;
+                if (code.Calls(typeFromHandle) && codes[i - 1].opcode == OpCodes.Ldtoken && codes[i - 1].OperandIs(typeof(Building_NutrientPasteDispenser)))
+                {
+                    yield return new CodeInstruction(OpCodes.Call, isDerivedFrom);
+                    i++;
+                }
+            }
+        }
+
+        public static bool IsDerivedFrom(Type type, Type toCheck) 
+        {
+            return toCheck.IsAssignableFrom(type);
+        }
+    }
+}
