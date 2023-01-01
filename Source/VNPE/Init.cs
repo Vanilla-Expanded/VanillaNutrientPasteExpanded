0149b800ac39c669469f612b8cf8d2a7a200aa24
KylianB <kylian.balde@gmail.com>
2023-01-01 19:19:52 +0100

    dripper auto-link to beds

--- Source/VNPE/Init.cs
+++ Source/VNPE/Init.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using RimWorld;
 using Verse;
 
 namespace VNPE
@@ -10,6 +11,23 @@ namespace VNPE
         {
             Harmony harmony = new Harmony("VanillaExpanded.VNutrientE");
             harmony.PatchAll();
+            // Add dripper to all beds
+            var defs = DefDatabase<ThingDef>.AllDefsListForReading;
+            foreach (var def in defs)
+            {
+                // Is it a bed?
+                if (def.building != null
+                    && def.building.buildingTags.Contains("Bed")
+                    && !def.defName.Contains("Spot")
+                    && def.GetCompProperties<CompProperties_AffectedByFacilities>() is CompProperties_AffectedByFacilities props
+                    && !props.linkableFacilities.Contains(VThingDefOf.VNPE_NutrientPasteDripper))
+                {
+                    props.linkableFacilities.Add(VThingDefOf.VNPE_NutrientPasteDripper);
+                }
+            }
+
+            var dripper = DefDatabase<ThingDef>.GetNamed("VNPE_NutrientPasteDripper");
+            dripper.GetCompProperties<CompProperties_Facility>().ResolveReferences(dripper);
         }
     }
 }
