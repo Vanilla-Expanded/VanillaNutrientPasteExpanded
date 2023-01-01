1247dbb1ea80d99129c02a08970a0293491e1aac
KylianB <kylian.balde@gmail.com>
2023-01-01 14:01:21 +0100

    remove unused code

--- Source/VNPE/VThingDefOf.cs
+++ Source/VNPE/VThingDefOf.cs
@@ -10,7 +10,6 @@ namespace VNPE
         public static ThingDef VNPE_NutrientPasteFeeder;
         public static ThingDef VNPE_NutrientPasteDripper;
         public static EffecterDef EatVegetarian;
-        public static JobDef VNPE_TakeFromTap;
 
         static VThingDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(VThingDefOf));
     }
