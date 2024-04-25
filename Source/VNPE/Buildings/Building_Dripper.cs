817e357d7e18088d21d90361ebcc270b3bfa1c1e
Kupie <kupokraft@gmail.com>
2024-04-24 19:20:25 -0500

    Update Building_Dripper.cs

--- Source/VNPE/Buildings/Building_Dripper.cs
+++ Source/VNPE/Buildings/Building_Dripper.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+using System.Linq;
 using PipeSystem;
 using RimWorld;
 using UnityEngine;
@@ -39,30 +39,26 @@ namespace VNPE
                     for (int o = 0; o < occupants.Count; o++)
                     {
                         var occupant = occupants[o];
-                        // Make sure we only apply effect to pawn right next to the dripper
-                        if (occupant.Position.AdjacentToCardinal(pos))
+                        if (occupant.needs.food.CurLevelPercentage <= 0.26)
                         {
-                            if (occupant.needs.food.CurLevelPercentage <= 0.26)
+                            net.DrawAmongStorage(1, net.storages);
+                            var meal = ThingMaker.MakeThing(ThingDefOf.MealNutrientPaste);
+                            if (meal.TryGetComp<CompIngredients>() is CompIngredients ingredients)
                             {
-                                net.DrawAmongStorage(1, net.storages);
-                                var meal = ThingMaker.MakeThing(ThingDefOf.MealNutrientPaste);
-                                if (meal.TryGetComp<CompIngredients>() is CompIngredients ingredients)
+                                for (int s = 0; s < net.storages.Count; s++)
                                 {
-                                    for (int s = 0; s < net.storages.Count; s++)
+                                    var parent = net.storages[s].parent;
+                                    if (parent.TryGetComp<CompRegisterIngredients>() is CompRegisterIngredients storageIngredients)
                                     {
-                                        var parent = net.storages[s].parent;
-                                        if (parent.TryGetComp<CompRegisterIngredients>() is CompRegisterIngredients storageIngredients)
-                                        {
-                                            for (int ig = 0; ig < storageIngredients.ingredients.Count; ig++)
-                                                ingredients.RegisterIngredient(storageIngredients.ingredients[ig]);
-                                        }
+                                        for (int ig = 0; ig < storageIngredients.ingredients.Count; ig++)
+                                            ingredients.RegisterIngredient(storageIngredients.ingredients[ig]);
                                     }
                                 }
-
-                                var ingestedNum = meal.Ingested(occupant, occupant.needs.food.NutritionWanted);
-                                occupant.needs.food.CurLevel += ingestedNum;
-                                occupant.records.AddTo(RecordDefOf.NutritionEaten, ingestedNum);
                             }
+
+                            var ingestedNum = meal.Ingested(occupant, occupant.needs.food.NutritionWanted);
+                            occupant.needs.food.CurLevel += ingestedNum;
+                            occupant.records.AddTo(RecordDefOf.NutritionEaten, ingestedNum);
                         }
                     }
                 }
